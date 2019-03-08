30f7a68effd0fe6ce71c11b858e1a164321bc894
Hemant tanmai <tanmai97@gmail.com>
2019-03-08 16:34:09 +0530

    Update loopname.py

--- loopname.py
+++ loopname.py
@@ -1,5 +1,6 @@
 import os 
 dir_path=[]
+result=[]
 def walklevel(some_dir, level=0):
     some_dir = some_dir.rstrip(os.path.sep)
     assert os.path.isdir(some_dir)
@@ -13,5 +14,8 @@ def walklevel(some_dir, level=0):
 dir_path= os.path.dirname(os.path.realpath('_file_'))
 for root,dirs,files in walklevel(os.getcwd()):
 	for rn in dirs:
-		print rn
+		result.append(os.path.join(root,rn))
+
+print result
 		
+
